d9da8530ffb19b115afa15d2996acbdfd38798ea
guruputran <gpsmartcodes@gmail.com>
2022-04-06 23:19:34 +0800

    angular 13 CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { DialogComponent } from './dialog/dialog.component'
+import { ApiService } from './services/api.service'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nghello';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'productName',
+    'category',
+    'date',
+    'freshness',
+    'comment',
+    'price',
+    'action'
+  ]
+  dataSource!: MatTableDataSource<any>
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+
+  title = 'nghello'
+  constructor (private dialog: MatDialog, private api: ApiService) {}
+  ngOnInit (): void {
+    this.getAllProducts()
+  }
+  openDialog () {
+    this.dialog
+      .open(DialogComponent, {
+        data: {},
+        width: '30%'
+      })
+      .afterClosed()
+      .subscribe(val => {
+        if (val === 'save') {
+          this.getAllProducts()
+        }
+      })
+  }
+  getAllProducts () {
+    this.api.getProduct().subscribe({
+      next: res => {
+        //  console.log(res);
+        this.dataSource = new MatTableDataSource(res)
+        this.dataSource.paginator = this.paginator
+        this.dataSource.sort = this.sort
+      },
+      error: err => {
+        console.log('Error is', err)
+        alert('Error while get the product')
+      }
+    })
+  }
+  editProduct (row: any) {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+        data: row
+      })
+      .afterClosed()
+      .subscribe(val => {
+        if (val === 'update') {
+          this.getAllProducts()
+        }
+      })
+  }
+  deleteProduct (id: number) {
+    this.api.deleteProduct(id).subscribe({
+      next: res => {
+        alert('Product is deleted')
+        this.getAllProducts()
+      },
+      error: () => {
+        alert('Error while deleting the record')
+      }
+    })
+  }
+  applyFilter (event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage()
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatRadioModule } from '@angular/material/radio'
+import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort'
+
+import { DialogComponent } from './dialog/dialog.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DialogComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonToggleModule,
+    MatRadioModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
